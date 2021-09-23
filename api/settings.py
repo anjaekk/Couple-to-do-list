d0289c7148de81ceafca752a814c24048150fc54
anjaekk <anjaekk@gmail.com>
2021-09-24 04:10:59 +0900

    Fix: password validation

--- api/settings.py
+++ api/settings.py
@@ -80,13 +80,7 @@ DATABASES = api_settings.DATABASES
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
